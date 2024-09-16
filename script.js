6fb4648b3a25e2e1792928bd11915ff2f50a5d97
harshtripathi6 <52310241+harshtripathi6@users.noreply.github.com>
2024-09-15 22:00:52 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Your message has been sent!');
+});
